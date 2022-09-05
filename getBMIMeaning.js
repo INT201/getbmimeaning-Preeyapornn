04a48cc8e2bbf363829b6f9a3d342d386b30be73
Preeyaporn Petcharat <preeyaporn.benz@mail.kmutt.ac.th>
2022-09-05 14:28:20 +0700

    add for testing

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -2,6 +2,8 @@ const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
   //code here
+
+  console.log("Hello orld")
 }
 function getBMIMeaning(weight, height) {
   //code here
